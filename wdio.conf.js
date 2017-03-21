8c7f9d94475396f3ed9a27a19e716fffc0183d6b
syusyu02 <syuduckdv@hotmail.com>
2017-03-21 09:10:45 +0800

    Change to run this project

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/duplicated-items/*mocha.js'
+        './test/specs/trial/*.js'
     ],
     // Patterns to exclude.
     exclude: [
